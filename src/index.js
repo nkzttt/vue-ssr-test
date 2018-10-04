1658367c43a4f733cc01b262787d8e16ac6ebdd8
nakazato hiroki <b10214@pc-10832.cyberagent.co.jp>
2018-10-04 17:21:26 +0900

    [clean] settle code around the router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
-import Router from 'vue-router';
 import App from './App.vue';
-import routes from './routes';
-
-Vue.use(Router);
-
-const router = new Router({ routes });
+import router from './router';
 
 new Vue({
   el: '#app',
--- src/router.js
+++ src/router.js
@@ -1,9 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
 import Top from './components/Top.vue';
 import Page from './components/Page.vue';
+
+Vue.use(Router);
 
 const routes = [
   { path: '/', component: Top },
   { path: '/page', component: Page }
 ];
 
-export default routes;
+const router = new Router({ routes });
+
+export default router;
